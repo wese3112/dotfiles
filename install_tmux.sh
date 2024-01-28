124f5f24f44debc2469cb8fa4d2c0a716d5db78a
wese3112 <wese3112@startmail.com>
2024-01-28 20:53:37 +0100

    fixed home paths in install_tmux.sh

--- install_tmux.sh
+++ install_tmux.sh
@@ -1,4 +1,4 @@
 sudo apt install -y tmux xsel
-git clone https://github.com/tmux-plugins/tpm /home/sebastian/.tmux/plugins/tpm
-mkdir -p /home/sebastian/.config/tmux
-ln -s /home/sebastian/Downloads/dotfiles/tmux.conf /home/sebastian/.config/tmux/tmux.conf
+git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
+mkdir -p $HOME/.config/tmux
+ln -s $HOME/Downloads/dotfiles/tmux.conf $HOME/.config/tmux/tmux.conf
