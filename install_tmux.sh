9cb4718d51d0d26919a6a52749d0b4c8adc9666b
Sebastian Werner <wese3112@startmail.com>
2024-01-28 20:43:47 +0100

    added tmux install scripts

---
+++ install_tmux.sh
@@ -0,0 +1,4 @@
+sudo apt install -y tmux xsel
+git clone https://github.com/tmux-plugins/tpm /home/sebastian/.tmux/plugins/tpm
+mkdir -p /home/sebastian/.config/tmux
+ln -s /home/sebastian/Downloads/dotfiles/tmux.conf /home/sebastian/.config/tmux/tmux.conf
